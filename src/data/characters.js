2ed6eaf4320091d8616a36dbaaae6007f61650a6
FelipeGdasilva <felipegomes35@yahoo.com.br>
2026-01-19 00:40:03 -0300

    adicionado o projeto

--- src/data/characters.js
+++ src/data/characters.js
@@ -5,62 +5,62 @@ const characters = [
     name: "Sonic the Hedgehog",
     icon: `${process.env.PUBLIC_URL}/icons/sonic-icon.png`,
     image: `${process.env.PUBLIC_URL}/images/sonic.png`,
-    sound: "/sounds/sonic.mp3"
+    sound: `${process.env.PUBLIC_URL}/sounds/sonic.mp3`
   },
   {
     id: 2,
     name: "Miles Tails Prower",
-    icon: "/icons/tails-icon.png",
-    image: "/images/tails.png",
+    icon: `${process.env.PUBLIC_URL}/icons/tails-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/tails.png`,
     sound: "/sounds/tails.mp3"
   },
   {
     id: 3,
     name: "Knuckles the Echidna",
-    icon: "/icons/knuckles-icon.png",
-    image: "/images/knuckles.png",
+    icon: `${process.env.PUBLIC_URL}/icons/knuckles-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/knuckles.png`,
     sound: "/sounds/kunckles.mp3"
   },
   {
     id: 4,
     name: "Shadow the Hedgehog",
-    icon: "/icons/shadow-icon.png",
-    image: "/images/shadow.png",
+    icon: `${process.env.PUBLIC_URL}/icons/shadow-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/shadow.png`,
     sound: "/sounds/shadow.mp3"
   },
   {
     id: 5,
     name: "Supersonic",
-    icon: "/icons/supersonic-icon.png",
-    image: "/images/supersonic.png",
+    icon: `${process.env.PUBLIC_URL} /icons/supersonic-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/supersonic.png`,
     sound: "/sounds/supersonic.mp3"
   },
   {
     id: 6,
     name: "Rouge the Bat",
-    icon: "/icons/rouge-icon.png",
-    image: "/images/rouge.png",
+    icon: `${process.env.PUBLIC_URL}/icons/rouge-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/rouge.png`,
     sound: "/sounds/rouge.mp3"
   },
   {
     id: 7,
     name: "Dr.Eggman",
-    icon: "/icons/eggman-icon.png",
-    image: "/images/eggman.png",
+    icon: `${process.env.PUBLIC_URL}/icons/eggman-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/eggman.png`,
     sound: "/sounds/eggman.mp3"
   },
   {
     id: 8,
     name: "amy Rose",
-    icon: "/icons/amy-icon.png",
-    image: "/images/amy.png",
+    icon: `${process.env.PUBLIC_URL}/icons/amy-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/amy.png`,
     sound: "/sounds/amy.mp3"
   },
   {
     id: 9,
     name: "cream the Rabbit",
-    icon: "/icons/cream-icon.png",
-    image: "/images/cream.png",
+    icon: `${process.env.PUBLIC_URL}/icons/cream-icon.png`,
+    image: `${process.env.PUBLIC_URL}/images/cream.png`,
     sound: "/sounds/cream.mp3"
   }
   
